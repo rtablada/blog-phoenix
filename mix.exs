3f922d23f63bb8fccab196abcaaf27291fb13438
Ryan Tablada <ryan.tablada@gmail.com>
2017-05-09 13:47:03 -0500

    try to fix node-sass error

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Blog.Mixfile do
 
   def project do
     [app: :blog,
-     version: "0.0.1",
+     version: "0.0.${committed_at}",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
@@ -11,6 +11,11 @@ defmodule Blog.Mixfile do
      start_permanent: Mix.env == :prod,
      aliases: aliases(),
      deps: deps()]
+  end
+
+  @doc "Unix timestamp of the last commit."
+  def committed_at do
+    System.cmd("git", ~w[log -1 --date=short --pretty=format:%ct]) |> elem(0)
   end
 
   # Configuration for the OTP application.
